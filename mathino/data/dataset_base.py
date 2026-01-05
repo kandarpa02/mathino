d38df038f2a179f7e61f8e7eb699b3ba1af25718
kandarpa02 <kandarpaexe@gmail.com>
2026-01-05 20:50:12 +0530

    layers, init

--- mathino/data/dataset_base.py
+++ mathino/data/dataset_base.py
@@ -1,11 +1,17 @@
 import os
 import gdown
 import numpy as np
+import os
+
+def cache_path(filename):
+    cache_dir = os.path.expanduser("~/.cache/mathino")
+    os.makedirs(cache_dir, exist_ok=True)
+    return os.path.join(cache_dir, filename)
 
 class dataset:
     def __init__(self, path, link):
         dirpath = os.path.dirname(path)
-        if dirpath and not os.path.exists(dirpath):
+        if dirpath:
             os.makedirs(dirpath, exist_ok=True)
 
         if not os.path.exists(path):
