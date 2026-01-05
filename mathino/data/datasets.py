ccc58c9099eaf54e102f8a0daa7423d043ae4352
kandarpa02 <kandarpaexe@gmail.com>
2026-01-05 20:40:45 +0530

    layers, init

--- mathino/data/datasets.py
+++ mathino/data/datasets.py
@@ -5,7 +5,7 @@ __all__ = ["MNIST"]
 class MNIST(dataset):
     def __init__(self, path=None):
         super().__init__(
-            path='mathino/data/storage/mnist.npz' if path==None else path, 
+            path='mathino/data/mnist.npz' if path==None else path, 
             link="https://drive.google.com/file/d/1Bmft6ApTEqadnWR5m-7Ozuy7x3pAnlYx/view?usp=sharing"
             )
     
